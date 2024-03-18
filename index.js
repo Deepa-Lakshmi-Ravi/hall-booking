ac612eb5f437d6d5757d8fe018a7f5425c423bdc
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-03-18 15:36:19 -0700

    some changes in logic

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ let rooms = [
     name: "Standard",
     seats: "200",
     price: "2000",
-    amenities: "wifi,non-ac,screen with projector",
+    amenities: ["wifi", "non-ac", "screen with projector"],
     roomID: "101",
     bookedDetails: [
       {
         customerName: "Deepa",
-        bookedDate: new Date("2023-11-20"),
-        startTime: "2023-11-20T08:30",
-        endTime: "2023-11-20T11:30",
+        bookingID: "1",
+        bookedDate: "2023-11-20",
+        startTime: "08:30",
+        endTime: "11:30",
         status: "confirmed",
         roomID: "101",
       },
@@ -27,14 +28,15 @@ let rooms = [
     name: "Elite",
     seats: "350",
     price: "3000",
-    amenities: "wifi,ac,screen with projector",
+    amenities: ["wifi", "ac", "screen with projector"],
     roomID: "102",
     bookedDetails: [
       {
         customerName: "Karthik",
-        bookedDate: new Date("2023-11-25"),
-        startTime: "2023-11-25T15:30",
-        endTime: "2023-11-25T17:30",
+        bookingID: "2",
+        bookedDate: "2023-11-25",
+        startTime: "15:30",
+        endTime: "17:30",
         status: "confirmed",
         roomID: "102",
       },
@@ -44,35 +46,56 @@ let rooms = [
     name: "Premium",
     seats: "450",
     price: "4000",
-    amenities: "wifi,ac,screen with projector",
+    amenities: ["wifi", "ac", "screen with projector"],
     roomID: "103",
     bookedDetails: [
       {
         customerName: "Sajeev",
-        bookedDate: new Date("2023-12-25"),
-        startTime: "2023-12-25T20:30",
-        endTime: "2023-12-25T22:30",
+        bookingID: "3",
+        bookedDate: "2023-12-25",
+        startTime: "20:30",
+        endTime: "22:30",
         status: "Payment_Pending",
         roomID: "103",
       },
     ],
   },
   {
-    name: "Standard",
+    name: "Deluxe",
     seats: "550",
     price: "5000",
-    amenities: "wifi,ac",
+    amenities: ["wifi", "ac", "entertainment(Tv/music)"],
     roomID: "104",
     bookedDetails: [
       {
         customerName: "Keerthika",
-        bookedDate: new Date("2024-01-02"),
-        startTime: "2024-01-02T20:30",
-        endTime: "2024-01-02T22:30",
+        bookingID: "4",
+        bookedDate: "2024-01-02",
+        startTime: "20:30",
+        endTime: "22:30",
         status: "Payment_Pending",
         roomID: "104",
       },
     ],
+  },
+  {
+    name: "Family Room",
+    seats: "600",
+    price: "3000",
+    amenities: ["free wi-fi", "non-ac", "entertainment(Tv/music"],
+    roomID: "105",
+  },
+  {
+    name: "Queen",
+    seats: "400",
+    price: "3500",
+    amenities: [
+      "free wi-fi",
+      "non-ac",
+      "double bed room",
+      "entertainment(Tv/music",
+    ],
+    roomID: "106",
   },
 ];
 
@@ -82,8 +105,8 @@ app.get("/", (req, res) => {
   });
 });
 app.get("/rooms/all", (req, res) => {
-  res.status(200).json({ RoomsList: rooms });
-  console.log(rooms);
+  const roomsList = rooms.map(({ bookedDetails, ...rooms }) => rooms);
+  res.status(200).json({ RoomsList: roomsList });
 });
 //create a Room
 app.post("/createRoom", (req, res) => {
@@ -102,8 +125,15 @@ app.post("/createRoom", (req, res) => {
 });
 //booking room
 app.post("/bookRoom", (req, res) => {
-  let { customerName, bookedDate, startTime, endTime, status, roomID } =
-    req.body;
+  let {
+    customerName,
+    bookingID,
+    bookedDate,
+    startTime,
+    endTime,
+    status,
+    roomID,
+  } = req.body;
 
   let startTS = Date.parse(startTime);
   let endTS = Date.parse(endTime);
@@ -124,6 +154,7 @@ app.post("/bookRoom", (req, res) => {
       if (!isBooked) {
         let tobeBooked = {
           customerName,
+          bookingID,
           bookedDate,
           startTime,
           endTime,
@@ -145,14 +176,73 @@ app.post("/bookRoom", (req, res) => {
 
 //list all rooms with booked data
 app.get("/listRooms", (req, res) => {
-  res.send(rooms);
+  const roomsWithBookings = rooms.filter((room) => room.bookedDetails);
+  res.send(roomsWithBookings);
 });
 
 //list all customers with booked data(room name included)
 app.get("/listCustomers", (req, res) => {
-  const customers = rooms.map((room) => {
-    return [...room.bookedDetails, { RoomName: room.name }];
+  const customers = {};
+
+  rooms.forEach((room) => {
+    if (room.bookedDetails) {
+      room.bookedDetails.forEach((booking) => {
+        const { customerName, bookingID, startTime, endTime, status, roomID } =
+          booking;
+        const roomName = room.name;
+
+        if (!customers[customerName]) {
+          customers[customerName] = [];
+        }
+        customers[customerName].push({
+          roomName,
+          customerName,
+          bookingID,
+          startTime,
+          endTime,
+          status,
+          roomID,
+        });
+      });
+    }
   });
   res.send(customers);
+});
+
+// list how many times a customer has booked the room with the required details
+app.get("/customerBookings", (req, res) => {
+  let customerBookings = {};
+
+  rooms.forEach((room) => {
+    if (room.bookedDetails) {
+      room.bookedDetails.forEach((booking) => {
+        const { customerName, bookingID, startTime, endTime, status, roomID } =
+          booking;
+        const booking_ID = booking.bookingID;
+        const bookingDate = booking.bookedDate;
+        const roomName = room.name;
+
+        if (!customerBookings[customerName]) {
+          customerBookings[customerName] = {
+            bookings: [],
+            count: 0,
+          };
+        }
+
+        customerBookings[customerName].bookings.push({
+          roomName,
+          customerName,
+          startTime,
+          endTime,
+          bookingID,
+          bookingDate,
+          status,
+        });
+        customerBookings[customerName].count++;
+      });
+    }
+  });
+
+  res.send(customerBookings);
 });
 app.listen(PORT, () => console.log("Server Started"));
